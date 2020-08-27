7f1d776d3019f1532febce8d7f6e953c48642789
Sasha7b9 <Sasha7b9@tut.by>
2020-08-27 09:24:47 +0300

    *

--- sources/Panel/src/stm32l0xx_it.h
+++ sources/Panel/src/stm32l0xx_it.h
@@ -2,14 +2,14 @@
 
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void SVC_Handler(void);
-void PendSV_Handler(void);
-void SysTick_Handler(void);
+    void NMI_Handler(void);
+    void HardFault_Handler(void);
+    void SVC_Handler(void);
+    void PendSV_Handler(void);
+    void SysTick_Handler(void);
 
 #ifdef __cplusplus
 }
