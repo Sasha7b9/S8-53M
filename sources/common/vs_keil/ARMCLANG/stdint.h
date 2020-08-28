ff1fe7dc796f02366f3552f05f889f70c5f00741
Sasha7b9 <Sasha7b9@tut.by>
2020-08-28 15:20:10 +0300

    Всё компилируется без ошибок

--- sources/common/vs_keil/ARMCLANG/stdint.h
+++ sources/common/vs_keil/ARMCLANG/stdint.h
@@ -8,3 +8,4 @@ typedef unsigned short     uint16_t;
 typedef int                int32_t;
 typedef unsigned int       uint32_t;
 typedef unsigned long long uint64_t;
+typedef unsigned int       uintptr_t;
