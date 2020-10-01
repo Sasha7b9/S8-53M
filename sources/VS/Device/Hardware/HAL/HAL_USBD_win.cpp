548e213030bec10f37b733a7dfa3926fe774f90d
Sasha7b9 <Sasha7b9@tut.by>
2020-10-01 21:35:34 +0300

    *

---
+++ sources/VS/Device/Hardware/HAL/HAL_USBD_win.cpp
@@ -0,0 +1,31 @@
+#include "defines.h"
+
+
+void HAL_USBD::Init()
+{
+
+}
+
+
+bool HAL_USBD::PrevSendingComplete()
+{
+    return false;
+}
+
+
+void HAL_USBD::Transmit(uint8 *, int)
+{
+
+}
+
+
+void HAL_USBD::Flush(uint8 *, int)
+{
+
+}
+
+
+void HAL_USBD::Wait()
+{
+
+}
