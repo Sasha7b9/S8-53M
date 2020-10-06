7af92d37976ff90cd39373a56f6e8b0ad47cb7d1
Sasha7b9 <Sasha7b9@tut.by>
2020-10-06 23:10:27 +0300

    *

---
+++ sources/VS/Device/src/Hardware/HAL/HAL_FSMC_win.cpp
@@ -0,0 +1,18 @@
+#include "defines.h"
+#include "common/Hardware/HAL/HAL.h"
+
+
+uint8 * const HAL_FSMC::ADDR_CDISPLAY = 0;
+uint8 *const HAL_FSMC::ADDR_FPGA = 0;
+
+
+uint8 HAL_FSMC::Read(uint8 const *const)
+{
+    return 0;
+}
+
+
+void HAL_FSMC::Write(uint8 *const, uint8)
+{
+
+}
