c109621b392f0105e67aec2ca37b3ac1fa88dc57
Sasha7b9 <Sasha7b9@tut.by>
2020-10-06 23:53:49 +0300

    *

---
+++ sources/VS/Device/src/Hardware/HAL/HAL_TIM2_win.cpp
@@ -0,0 +1,26 @@
+#include "defines.h"
+#include "common/Hardware/HAL/HAL.h"
+
+
+uint HAL_TIM2::GetTicks()
+{
+    return 0;
+}
+
+
+uint HAL_TIM2::TimeMS()
+{
+    return 0;
+}
+
+
+void HAL_TIM2::StartMultiMeasurement()
+{
+
+}
+
+
+void HAL_TIM2::Delay(uint)
+{
+
+}
