05418d15c32dec7f3eb76fabb7736f4e97bb6a33
Sasha7b9 <Sasha7b9@tut.by>
2020-10-06 11:39:46 +0300

    script clean для keilЭ

--- sources/scripts/Keil/clean.bat
+++ sources/scripts/Keil/clean.bat
@@ -1,23 +1,33 @@
 @echo off
 
 if "%1" EQU "" goto HINT
-if %1==device  ( call :CLEAN Device  & goto EXIT )
-if %1==dloader ( call :CLEAN DLoader & goto EXIT )
-if %1==panel   ( call :CLEAN Panel   & goto EXIT )
-if %1==all     ( call clean.bat device & call clean.bat dloader & goto EXIT )
+if %1==debug   ( call :CLEAN_DEBUG & goto EXIT )
+if %1==release ( call :CLEAN_RELEASE & goto EXIT )
+if %1==all     ( call clean.bat debug & call clean.bat release & goto EXIT )
 
 goto HINT
 
-:CLEAN
-    echo Clean %1
-    c:\Keil_v5\UV4\UV4 -c..\..\%1\%1.uvprojx -j0
+:CLEAN_DEBUG
+    echo Clean Debug Keil
+    %COMPILER% -c..\..\Device\Device-Debug.uvprojx -j0
+    %COMPILER% -c..\..\DLoader\DLoader-Debug.uvprojx -j0
+    %COMPILER% -c..\..\Panel\Panel-Debug.uvprojx -j0
+
+    exit /b
+
+:CLEAN_RELEASE
+    echo Clean Release Keil
+    %COMPILER% -c..\..\Device\Device.uvprojx -j0
+    %COMPILER% -c..\..\DLoader\DLoader.uvprojx -j0
+    %COMPILER% -c..\..\Panel\Panel.uvprojx -j0
+
     exit /b
 
 :HINT
 
     echo.
     echo Usage:
-    echo       clean [device^|dloader^|panel^|all]
+    echo       clean [debug^|release^|all]
     echo.
     goto EXIT
 
