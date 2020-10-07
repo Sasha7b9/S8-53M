53a5177536183ad8fd10369aafbe865de95d791a
Sasha7b9 <Sasha7b9@tut.by>
2020-10-07 14:50:13 +0300

    *

--- sources/Panel/src/Keyboard/Keyboard.cpp
+++ sources/Panel/src/Keyboard/Keyboard.cpp
@@ -15,11 +15,24 @@ static void DetectRegulator();
 
 #define BUTTON_IS_PRESS(state)  ((state) == 0)
 
-// При обнаружении нажатия кнопки сюда записывается время нажатия
-static uint timePress[NUM_SL][NUM_RL];
+#define KEY(sl, rl) (keys[sl][rl])
 
 
-static const Key::E keys[NUM_SL][NUM_RL] =     
+struct KeyboardStruct
+{
+    KeyboardStruct(Key::E k) : key(k), timePress(0U) { }
+    // Возвращает true, если нопка наодится в нажатом положении
+    bool IsPressed() const { return (timePress != 0); }
+    // Вовзращате true, если произошло длинное нажатие
+    bool HappenedLongPressed() const { return (timePress == UINT_MAX); }
+
+    bool IsValid() const { return (key != Key::None) && (key != Key::Count); }
+    Key::E key;
+    uint   timePress;   // Время нажатия кнопки
+};
+
+
+static KeyboardStruct keys[NUM_SL][NUM_RL] =     
 //    RL0                   RL1            RL2           RL3         RL4          RL5           RL6         RL7
     {{Key::ChannelA,        Key::Count,    Key::Count,   Key::Count, Key::Count,  Key::Count,   Key::Count, Key::Menu},  // SL0
      {Key::ChannelB,        Key::Count,    Key::Count,   Key::Count, Key::Count,  Key::Count,   Key::Count, Key::F1},    // SL1
@@ -28,8 +41,8 @@ static const Key::E keys[NUM_SL][NUM_RL] =
      {Key::Cursors,         Key::Measures, Key::Display, Key::Help,  Key::Memory, Key::Service, Key::None,  Key::F4},    // SL4
      {Key::None,            Key::Count,    Key::Count,   Key::Count, Key::None,   Key::None,    Key::None,  Key::F5}};   // SL5
 
-static Pin *sls[] = { &pinSL0, &pinSL1, &pinSL2, &pinSL3, &pinSL4, &pinSL5 };
-static Pin *rls[] = { &pinRL0, &pinRL1, &pinRL2, &pinRL3, &pinRL4, &pinRL5, &pinRL6, &pinRL7 };
+static Pin *sls[NUM_SL] = { &pinSL0, &pinSL1, &pinSL2, &pinSL3, &pinSL4, &pinSL5 };
+static Pin *rls[NUM_RL] = { &pinRL0, &pinRL1, &pinRL2, &pinRL3, &pinRL4, &pinRL5, &pinRL6, &pinRL7 };
 
 #define SET_ALL_SL      pinSL0.Set(); pinSL1.Set(); pinSL2.Set(); pinSL3.Set(); pinSL4.Set(); pinSL5.Set()
 #define SET_SL(n)       sls[n]->Set();
@@ -42,14 +55,6 @@ static bool init = false;
 
 void Keyboard::Init()
 {
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 6; j++)
-        {
-            timePress[i][j] = 0;
-        }
-    }
-
     pointer = 0;
 
     HAL_TIM2::Init(&Keyboard::Update);
@@ -75,33 +80,33 @@ void Keyboard::Update()
         {
             uint state = READ_RL(rl);
 
-            Key::E key = keys[rl][sl];
+            KeyboardStruct &key = KEY(sl, rl);
 
-            if (key != Key::None)
+            if (key.IsValid())
             {
-                if ((timePress[rl][sl] != 0) && (timePress[rl][sl] != UINT_MAX))    // Если клавиша находится в нажатом положении
+                if (key.IsPressed() && !key.HappenedLongPressed())                  // Если клавиша находится в нажатом положении
                 {
-                    uint delta = time - timePress[rl][sl];
+                    uint delta = time - key.timePress;
                     if(delta > 500)                                                 // Если прошло более 500 мс с момента нажатия -
                     {
-                        timePress[rl][sl] = UINT_MAX;
-                        Buffer::AppendEvent(keys[rl][sl], Action::Long);                    // это будет длинное нажатие
+                        key.timePress = UINT_MAX;
+                        Buffer::AppendEvent(key.key, Action::Long);                 // это будет длинное нажатие
                     }
                     else if (delta > 100 &&                                         // Если прошло более 100 мс с момента нажатия
                         !BUTTON_IS_PRESS(state))                                    // и сейчас кнопка находится в отжатом состоянии
                     {
-                        timePress[rl][sl] = UINT_MAX;                               // То учитываем это в массиве
-                        Buffer::AppendEvent(keys[rl][sl], Action::Up);                      // И сохраняем отпускание кнопки в буфере команд
+                        key.timePress = UINT_MAX;                                   // То учитываем это в массиве
+                        Buffer::AppendEvent(key.key, Action::Up);                   // И сохраняем отпускание кнопки в буфере команд
                     }
                 }
-                else if (BUTTON_IS_PRESS(state) && timePress[rl][sl] != UINT_MAX)   // Если кнопка нажата
+                else if (BUTTON_IS_PRESS(state) && !key.HappenedLongPressed())      // Если кнопка нажата
                 {
-                    timePress[rl][sl] = time;                                       // то сохраняем время её нажатия
-                    Buffer::AppendEvent(keys[rl][sl], Action::Down);
+                    key.timePress = time;                                           // то сохраняем время её нажатия
+                    Buffer::AppendEvent(key.key, Action::Down);
                 }
-                else if(!BUTTON_IS_PRESS(state) && timePress[rl][sl] == UINT_MAX)
+                else if(!BUTTON_IS_PRESS(state) && key.HappenedLongPressed())
                 {
-                    timePress[rl][sl] = 0;
+                    key.timePress = 0;
                 }
             }
         }
@@ -122,20 +127,14 @@ static void DetectRegulator()
     static uint timePrevPress = 0;
     static bool needDetectButton = true;
     
-    bool press = false; // HAL_PIO::ReadPin('C', HPin::_2) ? false : true;
-
-    if(!press)
-    {
-        needDetectButton = true;
-    }
+    needDetectButton = true;
 
     if(needDetectButton)
     {
         uint time = TIME_MS;
 
-        if(press && prevPressButton && time - timePrevPress > 500)          // Если нажатие длится более 0.5 сек
+        if(prevPressButton && time - timePrevPress > 500)          // Если нажатие длится более 0.5 сек
         {
-//            Keyboard::AppendEvent(Key::RegButton, Action::Long);                                     // посылаем длинное нажатие
             needDetectButton = false;
             prevPressButton = false;
             timePrevPress = 0;
@@ -143,23 +142,15 @@ static void DetectRegulator()
 
         if(timePrevPress == 0)                                              // Если кнопка находилась ранее в отжатом положении
         {
-            if(press)                                                       // И теперь нажата
-            {
-                timePrevPress = time;
-                prevPressButton = true;
-//                Keyboard::AppendEvent(Key::RegButton, Action::Down);
-            }
+            timePrevPress = time;
+            prevPressButton = true;
         }
         else                                                                // Ексли копка была нажата ранее
         {
             if(time - timePrevPress > 50)                                   // то проверять в следующий раз будем не ранее чем через 50 мс
             {                                                               // во избежание дребезга контактов
-                if(!press)
-                {
-//                    Keyboard::AppendEvent(Key::RegButton, Action::Up);
-                    timePrevPress = 0;
-                    prevPressButton = false;
-                }
+                timePrevPress = 0;
+                prevPressButton = false;
             }
         }
     }
