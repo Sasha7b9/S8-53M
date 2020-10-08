9a8f8777d682e2c3bef793c3197d2761c3aefaad
Sasha7b9 <Sasha7b9@tut.by>
2020-10-08 10:43:32 +0300

    *

--- sources/Device/src/Settings/SettingsTypes.h
+++ sources/Device/src/Settings/SettingsTypes.h
@@ -1,44 +1,6 @@
 #pragma once
 #include "common/Settings/cSettingsTypes.h"
 
-
 const char *RangeName(Range::E range);
 
 const char *TBaseName(TBase::E tBase);
-
-
-// Сжимать ли сигналы при выводе измерений.
-struct ModeViewSignals { enum E
-{
-    AsIs,       // Показывать сигналы как есть
-    Compress    // Сжимать сетку с сигналами
-};};
-
-
-// Math
-
-
-struct ScaleMath { enum E
-{
-    Channel0,     // Масштаб берётся из канала 1
-    Channel1,     // Масштаб берётся из канала 2
-    Hand          // Масштаб задаётся вручную.
-};};
-
-
-// Тип балансировки АЦП каналов.
-// Дело в том, что уровни АЦП не совпадают из-за отличия характеристик ( ? ), поэтому мы вводим дополнительное смещение для одного из АЦП канала.
-struct BalanceADCtype { enum E
-{
-    Disable,     // Балансировка выключена.
-    Settings,    // Используются значения балансировки, которые получены автоматически.
-    Hand         // Используются значения балансировки, заданные вручную.
-};};
-
-// Тип дополнительного смещения
-struct RShiftADCtype { enum E
-{
-    Disable,
-    Settings,
-    Hand
-};};
