e30e22b353bb78c4549503b30ccf86f765eaaf87
Sasha7b9 <Sasha7b9@tut.by>
2020-10-08 10:40:36 +0300

    refact

--- sources/DLoader/src/Settings/SettingsTypes.h
+++ sources/DLoader/src/Settings/SettingsTypes.h
@@ -1,46 +1,6 @@
 #pragma once
 
 
-// Масштаб по времени
-struct TBase { enum E
-{
-    _2ns,
-    _5ns,
-    _10ns,
-    _20ns,
-    _50ns,
-    _100ns,
-    _200ns,
-    _500ns,
-    _1us, 
-    _2us,
-    _5us,
-    _10us,
-    _20us,
-    _50us,
-    _100us,
-    _200us,
-    _500us,
-    _1ms,
-    _2ms,
-    _5ms,
-    _10ms,
-    _20ms,
-    _50ms,
-    _100ms,
-    _200ms,
-    _500ms,
-    _1s,
-    _2s,
-    _5s,
-    _10s,
-    Count
-}; };
-
-
-// Cursors
-
-
 // Каким курсором управлять
 struct CursCntrl { enum E
 {
--- sources/Device/src/Settings/SettingsTypes.h
+++ sources/Device/src/Settings/SettingsTypes.h
@@ -2,54 +2,9 @@
 #include "common/Settings/cSettingsTypes.h"
 
 
-// ChannelX
-
 const char *RangeName(Range::E range);
 
-// Time
-
-
-// Масштаб по времени
-struct TBase { enum E
-{
-    _2ns,
-    _5ns,
-    _10ns,
-    _20ns,
-    _50ns,
-    _100ns,
-    _200ns,
-    _500ns,
-    _1us,
-    _2us,
-    _5us,
-    _10us,
-    _20us,
-    _50us,
-    _100us,
-    _200us,
-    _500us,
-    _1ms,
-    _2ms,
-    _5ms,
-    _10ms,
-    _20ms,
-    _50ms,
-    _100ms,
-    _200ms,
-    _500ms,
-    _1s,
-    _2s,
-    _5s,
-    _10s,
-    Count
-}; };
-
 const char *TBaseName(TBase::E tBase);
-
-
-
-// Measures
 
 
 // Сжимать ли сигналы при выводе измерений.
