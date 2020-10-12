7d0f32e56c955539f769f117f23e082a393319b6
Sasha7b9 <Sasha7b9@tut.by>
2020-10-12 11:36:30 +0300

    Дорабатаны скрипты Keil для конфигураций проектов

--- sources/scripts/Keil/build.bat
+++ sources/scripts/Keil/build.bat
@@ -32,8 +32,8 @@ goto HINT
 
 :BUILD_COMMON
     echo Keil : Build %1 %2
-    if %2==Debug   ( %COMPILER% -b ..\..\%1\%1-Debug.uvprojx -j0 -z -o %1.out)
-    if %2==Release ( %COMPILER% -b ..\..\%1\%1.uvprojx -j0 -z -o %1.out)
+    if %2==Debug   ( %COMPILER% -b ..\..\%1\%1.uvprojx -t"Debug" -j0 -z -o %1.out)
+    if %2==Release ( %COMPILER% -b ..\..\%1\%1.uvprojx -t"Release" -j0 -z -o %1.out)
     set BUILD_STATUS=%ERRORLEVEL%
     if %BUILD_STATUS%==0 goto BUILD_SUCCESS
     echo ERROR!!! %1 failed !!!
