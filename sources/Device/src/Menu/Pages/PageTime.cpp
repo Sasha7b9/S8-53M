6105035ca7e5a63146a1089b7d2687bca3fbc1ac
Sasha7b9 <Sasha7b9@tut.by>
2020-10-20 17:08:06 +0300

    *

--- sources/Device/src/Menu/Pages/PageTime.cpp
+++ sources/Device/src/Menu/Pages/PageTime.cpp
@@ -5,40 +5,12 @@
 #include "Menu/Pages/Definition.h"
 #include "Settings/Settings.h"
 
+static bool IsActive_Sample(void)
+{
+    return sTime_RandomizeModeEnabled();
+}
 
-extern const Page pTime;
-
-
-extern const Choice mcSample;           // РАЗВЕРТКА - Выборка
-static bool  IsActive_Sample();
-extern const Choice mcPeakDet;          // РАЗВЕРТКА - Пик дет
-static bool  IsActive_PeakDet();
-void        OnChanged_PeakDet(bool active);
-extern const Choice mcTPos;             // РАЗВЕРТКА - To
-void        OnChanged_TPos(bool active);
-extern const Choice mcSelfRecorder;     // РАЗВЕРТКА - Самописец
-static bool  IsActive_SelfRecorder();
-extern const Choice mcDivRole;          // РАЗВЕРТКА - Ф-ция ВР/ДЕЛ
-
-
-// РАЗВЕРТКА ///////////////////
-DEF_PAGE_5(pTime, PageMain::self, NamePage::Time,
-    "РАЗВЕРТКА", "SCAN",
-    "Содержит настройки развёртки.",
-    "Contains scan settings.",
-    mcSample,
-    mcPeakDet,
-    mcTPos,
-    mcSelfRecorder,
-    mcDivRole,
-    nullptr, nullptr, nullptr, nullptr
-)
-
-const Page *PageTime::self = &pTime;
-
-
-// РАЗВЕРТКА - Выборка -------------------------------------------------------------------------------------------------------------------------------
-DEF_CHOICE_2(mcSample, &pTime,
+DEF_CHOICE_2(mcSample, PageTime::self,
     "Выборка", "Sampling",
     "\"Реальная\" - \n"
     "\"Эквивалентная\" -"
@@ -47,24 +19,8 @@ DEF_CHOICE_2(mcSample, &pTime,
     "\"Equals\" - "
     ,
     "Реальное время", "Real",
-    "Эквивалентная",  "Equals",
+    "Эквивалентная", "Equals",
     SAMPLE_TYPE, IsActive_Sample, nullptr, nullptr
-)
-
-
-static bool IsActive_Sample(void)
-{
-    return sTime_RandomizeModeEnabled();
-}
-
-// РАЗВЕРТКА - Пик дет -------------------------------------------------------------------------------------------------------------------------------
-DEF_CHOICE_2(mcPeakDet, &pTime,
-    "Пик дет", "Pic deat",
-    "Включает/выключает пиковый детектор.",
-    "Turns on/off peak detector.",
-    DISABLE_RU, DISABLE_EN,
-    ENABLE_RU,  ENABLE_EN,
-    PEAKDET, IsActive_PeakDet, OnChanged_PeakDet, nullptr
 )
 
 static bool IsActive_PeakDet(void)
@@ -114,15 +70,13 @@ void OnChanged_PeakDet(bool active)
     }
 }
 
-// РАЗВЕРТКА - To ------------------------------------------------------------------------------------------------------------------------------------
-DEF_CHOICE_3(mcTPos, &pTime,
-    "\x7b", "\x7b",
-    "Задаёт точку привязки нулевого смещения по времени к экрану - левый край, центр, правый край.",
-    "Sets the anchor point nuleovgo time offset to the screen - the left edge, center, right edge.",
-    "Лево",  "Left",
-    "Центр", "Center",
-    "Право", "Right",
-    SET_TPOS, nullptr, OnChanged_TPos, nullptr
+DEF_CHOICE_2(mcPeakDet, PageTime::self,
+    "Пик дет", "Pic deat",
+    "Включает/выключает пиковый детектор.",
+    "Turns on/off peak detector.",
+    DISABLE_RU, DISABLE_EN,
+    ENABLE_RU, ENABLE_EN,
+    PEAKDET, IsActive_PeakDet, OnChanged_PeakDet, nullptr
 )
 
 void OnChanged_TPos(bool active)
@@ -131,14 +85,14 @@ void OnChanged_TPos(bool active)
     FPGA::SetTShift(TSHIFT);
 }
 
-// РАЗВЕРТКА - Самописец -----------------------------------------------------------------------------------------------------------------------------
-DEF_CHOICE_2(mcSelfRecorder, &pTime,
-    "Самописец", "Self-Recorder",
-    "Включает/выключает режим самописца. Этот режим доступен на развёртках 20мс/дел и более медленных.",
-    "Turn on/off the recorder. This mode is available for scanning 20ms/div and slower.",
-    DISABLE_RU, DISABLE_EN,
-    ENABLE_RU,  ENABLE_EN,
-    SET_SELFRECORDER, IsActive_SelfRecorder, nullptr, nullptr
+DEF_CHOICE_3(mcTPos, PageTime::self,
+    "\x7b", "\x7b",
+    "Задаёт точку привязки нулевого смещения по времени к экрану - левый край, центр, правый край.",
+    "Sets the anchor point nuleovgo time offset to the screen - the left edge, center, right edge.",
+    "Лево", "Left",
+    "Центр", "Center",
+    "Право", "Right",
+    SET_TPOS, nullptr, OnChanged_TPos, nullptr
 )
 
 static bool IsActive_SelfRecorder(void)
@@ -146,8 +100,16 @@ static bool IsActive_SelfRecorder(void)
     return SET_TBASE >= MIN_TBASE_P2P;
 }
 
-// РАЗВЕРТКА - Ф-ция ВР/ДЕЛ --------------------------------------------------------------------------------------------------------------------------
-DEF_CHOICE_2(mcDivRole, &pTime,
+DEF_CHOICE_2(mcSelfRecorder, PageTime::self,
+    "Самописец", "Self-Recorder",
+    "Включает/выключает режим самописца. Этот режим доступен на развёртках 20мс/дел и более медленных.",
+    "Turn on/off the recorder. This mode is available for scanning 20ms/div and slower.",
+    DISABLE_RU, DISABLE_EN,
+    ENABLE_RU, ENABLE_EN,
+    SET_SELFRECORDER, IsActive_SelfRecorder, nullptr, nullptr
+)
+
+DEF_CHOICE_2(mcDivRole, PageTime::self,
     "Ф-ция ВР/ДЕЛ", "Func Time/DIV"
     ,
     "Задаёт функцию для ручки ВРЕМЯ/ДЕЛ: в режиме сбора информации (ПУСК/СТОП в положении ПУСК):\n"
@@ -158,7 +120,21 @@ DEF_CHOICE_2(mcDivRole, &pTime,
     "1. \"Time\" - change the time shift.\n"
     "2. \"Memory\" - moving from memory."
     ,
-    "Время",  "Time",
+    "Время", "Time",
     "Память", "Memory",
     TIME_DIV_XPOS, nullptr, nullptr, nullptr
 )
+
+DEF_PAGE_5(pTime, PageMain::self, NamePage::Time,
+    "РАЗВЕРТКА", "SCAN",
+    "Содержит настройки развёртки.",
+    "Contains scan settings.",
+    mcSample,
+    mcPeakDet,
+    mcTPos,
+    mcSelfRecorder,
+    mcDivRole,
+    nullptr, nullptr, nullptr, nullptr
+)
+
+const Page *PageTime::self = &pTime;
