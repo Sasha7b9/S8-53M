5e23555904b8ada58c08d33980659f47e643e4d5
Sasha7b9 <Sasha7b9@tut.by>
2020-12-10 15:12:59 +0300

    warnings PVS

--- sources/common/Hardware/HAL/HAL_FSMC_c.cpp
+++ sources/common/Hardware/HAL/HAL_FSMC_c.cpp
@@ -38,34 +38,63 @@ void HAL_FSMC::Write(uint8 * const address, uint8 value)
 
 void HAL_FSMC::Init()
 {
-    InitRAM();
+    __HAL_RCC_FMC_CLK_ENABLE(); //-V2571
+    __HAL_RCC_GPIOD_CLK_ENABLE(); //-V2571
+    __HAL_RCC_GPIOE_CLK_ENABLE(); //-V2571
+    __HAL_RCC_GPIOF_CLK_ENABLE(); //-V2571
+    __HAL_RCC_GPIOG_CLK_ENABLE(); //-V2571
+
+
+    //           NOE          NWE          NE1
+    is.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_7;
+    HAL_GPIO_Init(GPIOD, &is); //-V2571
+
+    //           NE2
+    is.Pin = GPIO_PIN_9;
+    HAL_GPIO_Init(GPIOG, &is); //-V2571
+
+    //           D2           D3           D0            D1
+    is.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_14 | GPIO_PIN_15;
+    HAL_GPIO_Init(GPIOD, &is); //-V2571
+
+    //           D4           D5           D6           D7            D8            D9            D10           D11           D12
+    is.Pin = GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+    HAL_GPIO_Init(GPIOE, &is); //-V2571
+
+    //           D13          D14          D15
+    is.Pin = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10;
+    HAL_GPIO_Init(GPIOD, &is); //-V2571
+
+    //           A0           A1           A2           A3           A4           A5           A6            A7            A8            A9
+    is.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+    HAL_GPIO_Init(GPIOF, &is); //-V2571
+
+    //           A10          A11         A12           A13          A14         A15
+    is.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
+    HAL_GPIO_Init(GPIOG, &is); //-V2571
+
+    //           A16          A17            A18
+    is.Pin = GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13;
+    HAL_GPIO_Init(GPIOD, &is); //-V2571
+
+    //           A19          A20
+    is.Pin = GPIO_PIN_3 | GPIO_PIN_4;
+    HAL_GPIO_Init(GPIOD, &is); //-V2571
+
     InitFPGA();
+
+    InitRAM();
 }
 
 
-void HAL_FSMC::InitRAM()
+void HAL_FSMC::InitFPGA()
 {
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-    __HAL_RCC_GPIOG_CLK_ENABLE();
-
-    //           A16           A17           A18 
-    is.Pin = GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13;
-    HAL_GPIO_Init(GPIOD, &is);
-
-    //           A10          A11         A12           A13          A14         A15           NE3
-    is.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_10;
-    HAL_GPIO_Init(GPIOG, &is);
-
-    //           A6            A7          A8            A9
-    is.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-    HAL_GPIO_Init(GPIOF, &is);
-
     static SRAM_HandleTypeDef gSramHandle =
     {
-        FMC_NORSRAM_DEVICE,
-        FMC_NORSRAM_EXTENDED_DEVICE,
+        FMC_NORSRAM_DEVICE, //-V2571
+        FMC_NORSRAM_EXTENDED_DEVICE, //-V2571
         {
-            FMC_NORSRAM_BANK3,                 // Init.NSBank
+            FMC_NORSRAM_BANK1,                 // Init.NSBank
             FMC_DATA_ADDRESS_MUX_DISABLE,      // Init.DataAddressMux
             FMC_MEMORY_TYPE_NOR,               // Init.MemoryType
             FMC_NORSRAM_MEM_BUS_WIDTH_8,       // Init.MemoryDataWidth
@@ -93,13 +122,55 @@ void HAL_FSMC::InitRAM()
         FMC_ACCESS_MODE_C  // FSMC_AccessMode
     };
 
-    FMC_NORSRAM_TimingTypeDef *timing = const_cast<FMC_NORSRAM_TimingTypeDef *>(&sramTiming);
+    SRAM_HandleTypeDef *hsram = &gSramHandle;
+    FMC_NORSRAM_TimingTypeDef *Timing = const_cast<FMC_NORSRAM_TimingTypeDef *>(&sramTiming); //-V2567
 
-    HAL_SRAM_Init(&gSramHandle, timing, timing);
+    FMC_NORSRAM_Init(hsram->Instance, &(hsram->Init));
+
+    FMC_NORSRAM_Timing_Init(hsram->Instance, Timing, hsram->Init.NSBank);
+
+    hsram->Extended->BWTR[hsram->Init.NSBank] = 0x0FFFFFFFU;
+
+    __FMC_NORSRAM_ENABLE(hsram->Instance, hsram->Init.NSBank);
 }
 
 
-void HAL_FSMC::InitFPGA()
+void HAL_FSMC::InitRAM()
 {
+    static SRAM_HandleTypeDef gSramHandle =
+    {
+        FMC_NORSRAM_DEVICE, //-V2571
+        FMC_NORSRAM_EXTENDED_DEVICE, //-V2571
+        {
+            FMC_NORSRAM_BANK2,                 // Init.NSBank
+            FMC_DATA_ADDRESS_MUX_DISABLE,      // Init.DataAddressMux
+            FMC_MEMORY_TYPE_NOR,               // Init.MemoryType
+            FMC_NORSRAM_MEM_BUS_WIDTH_16,      // Init.MemoryDataWidth
+            FMC_BURST_ACCESS_MODE_DISABLE,     // Init.BurstAccessMode
+            FMC_WAIT_SIGNAL_POLARITY_LOW,      // Init.WaitSignalPolarity
+            FMC_WRAP_MODE_DISABLE,             // Init.WrapMode
+            FMC_WAIT_TIMING_BEFORE_WS,         // Init.WaitSignalActive
+            FMC_WRITE_OPERATION_ENABLE,        // Init.WriteOperation
+            FMC_WAIT_SIGNAL_DISABLE,           // Init.WaitSignal
+            FMC_EXTENDED_MODE_DISABLE,         // Init.ExtendedMode
+            FMC_ASYNCHRONOUS_WAIT_DISABLE,     // Init.AsynchronousWait
+            FMC_WRITE_BURST_DISABLE            // Init.WriteBurst
+        }
+    };
 
+    static const FMC_NORSRAM_TimingTypeDef sramTiming =
+    {
+        0,                 // FSMC_AddressSetupTime
+        0,                 // FSMC_AddressHoldTime
+        6,                 // FSMC_DataSetupTime   При значении 9 32кБ записываются в RAM за 1000мкс. Уменьшение
+                           // на одну единцу уменьшает этот параметр на 90 мкс. Если 3 - 32кБ запишутся за 460 мкс.
+        0,                 // FSMC_BusTurnAroundDuration
+        0,                 // FSMC_CLKDivision
+        0,                 // FSMC_DataLatency
+        FMC_ACCESS_MODE_C  // FSMC_AccessMode
+    };
+
+    FMC_NORSRAM_TimingTypeDef *timing = const_cast<FMC_NORSRAM_TimingTypeDef *>(&sramTiming); //-V2567
+
+    HAL_SRAM_Init(&gSramHandle, timing, timing);
 }
