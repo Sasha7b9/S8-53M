d04a611316f01be9e00a8fdc171d988995fc49e7
Sasha7b9 <Sasha7b9@tut.by>
2021-02-26 09:29:41 +0300

    На панели должны зажигаться лампочки при нажатии кнопки и гаснуть при отпускании

--- sources/Panel/src/Interface/Interface.cpp
+++ sources/Panel/src/Interface/Interface.cpp
@@ -8,14 +8,31 @@
 // Посылает следующее событие клавиатуры в SPI
 static void SendKeyboardEvent(KeyboardEvent event);
 
+// Зажечь все светодиоды
+static void TurnOnLEDS();
+
+// Потушить все светодиоды
+static void TurnOffLEDS();
+
 
 void Interface::Update()
 {
     while (!Keyboard::Buffer::IsEmpty())
     {
+        KeyboardEvent event = Keyboard::Buffer::GetNextEvent();
+
+        if (event.action == Action::Down)
+        {
+            TurnOffLEDS();
+        }
+        else
+        {
+            TurnOnLEDS();
+        }
+
+
         if (pinON.Read() == 0)
         {
-            KeyboardEvent event = Keyboard::Buffer::GetNextEvent();
             if (event.key == Key::Power)
             {
                 pinON.Set();
@@ -28,9 +45,28 @@ void Interface::Update()
     }
 }
 
+
 static void SendKeyboardEvent(KeyboardEvent event)
 {
     uint8 message[3] = { 0xFF, static_cast<uint8>(event.key), static_cast<uint8>(event.action) };
 
     HAL_SPI2::Send(message, 3);
+}
+
+
+static void TurnOnLEDS()
+{
+    led_ChannelA.Enable();
+    led_ChannelB.Enable();
+    led_Trig.Enable();
+    led_Set.Enable();
+}
+
+
+static void TurnOffLEDS()
+{
+    led_ChannelA.Disable();
+    led_ChannelB.Disable();
+    led_Trig.Disable();
+    led_Set.Disable();
 }
