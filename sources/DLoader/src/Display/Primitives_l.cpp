122535a8e8b0b8be0e5454688e206a05f3eac235
Sasha7b9 <Sasha7b9@tut.by>
2021-03-03 13:56:54 +0300

    keil и vs_keil компилируется без предупреждений

---
+++ sources/DLoader/src/Display/Primitives_l.cpp
@@ -0,0 +1,9 @@
+// 2021/03/03 13:54:08 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "common/Display/Primitives_.h"
+
+
+void Primitives::HLine::Draw(int , int , int , const Color &)
+{
+
+}
