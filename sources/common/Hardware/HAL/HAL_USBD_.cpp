71665d2969d2f299f56943084ffa00f0a343a8d0
Sasha7b9 <Sasha7b9@tut.by>
2021-03-05 16:21:20 +0300

    *

--- sources/common/Hardware/HAL/HAL_USBD_.cpp
+++ sources/common/Hardware/HAL/HAL_USBD_.cpp
@@ -14,7 +14,7 @@ void *HAL_USBD::handle = &handleUSBD;
 void HAL_USBD::Init()
 {
     USBD_Init(&handleUSBD, &VCP_Desc, 0);
-//    USBD_RegisterClass(&handleUSBD, &USBD_CDC);
+    USBD_RegisterClass(&handleUSBD, &USBD_CDC);
 //    USBD_CDC_RegisterInterface(&handleUSBD, &USBD_CDC_fops);
 //    USBD_Start(&handleUSBD);
 }
