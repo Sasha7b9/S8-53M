98858db277d87438861b9b1908c0aef857f05c66
Sasha7b9 <Sasha7b9@tut.by>
2021-03-16 12:29:18 +0300

    Refact

--- sources/common/Utils/Math_.h
+++ sources/common/Utils/Math_.h
@@ -39,8 +39,6 @@ namespace Math
     // используется большее в смысле модуля число.
     bool    FloatsIsEquals(float value0, float value1, float epsilonPart);
 
-    float   MinFrom3float(float value1, float value2, float value3);
-
     float   RandFloat(float min, float max);
 
     int8    AddInt8WithLimitation(int8 value, int8 delta, int8 min, int8 max);
