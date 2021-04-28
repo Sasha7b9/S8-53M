10bfbc5dcdba34233036c3b8495e9b91bdfa67b2
Sasha7b9 <Sasha7b9@tut.by>
2021-04-28 14:23:56 +0300

    *

--- sources/common/Hardware/Memory/Sector_.h
+++ sources/common/Hardware/Memory/Sector_.h
@@ -34,7 +34,7 @@ struct PacketROM
     bool IsValid() const { return (state == STATE_VALID); }
 
     uint Address() const {
-#ifdef GCC
+#ifdef __linux__
         return 0;
 #else
         return (uint)this;
