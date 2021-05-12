c566aabb85f7c23f51894691ee1934c157894e75
Sasha7b9 <Sasha7b9@tut.by>
2021-05-12 12:12:39 +0300

    refact

--- sources/Device/src/Menu/Pages/Debug/PageBalanceADC.cpp
+++ sources/Device/src/Menu/Pages/Debug/PageBalanceADC.cpp
@@ -11,7 +11,7 @@ static int16 shiftADCA;
 static int16 shiftADCB;
 
 
-static void Draw_ADC_Balance_Mode(int, int)
+static void Draw_Mode(int, int)
 {
     int8 shift[2][3] =
     {
@@ -24,59 +24,59 @@ static void Draw_ADC_Balance_Mode(int, int)
 }
 
 
-static void OnChanged_ADC_Balance_Mode(bool)
+static void OnChanged_Mode(bool)
 {
-    Draw_ADC_Balance_Mode(0, 0);
+    Draw_Mode(0, 0);
 
     //    FPGA::WriteToHardware(WR_ADD_RSHIFT_DAC1, (uint8)shiftADCA, false);
     //    FPGA::WriteToHardware(WR_ADD_RSHIFT_DAC2, (uint8)shiftADCB, false);
 }
 
 
-DEF_CHOICE_3(mcADC_Balance_Mode, PageDebug::PageADC::PageBalance::self,
+DEF_CHOICE_3(mcMode, PageDebug::PageADC::PageBalance::self,
     "Режим", "Mode",
     "",
     "",
     DISABLE_RU, DISABLE_EN,
     "Реальный", "Real",
     "Ручной", "Manual",
-    setNRST.balance_ADC_type, nullptr, OnChanged_ADC_Balance_Mode, Draw_ADC_Balance_Mode
+    setNRST.balance_ADC_type, nullptr, OnChanged_Mode, Draw_Mode
 )
 
 
-static bool IsActive_ADC_Balance_Shift()
+static bool IsActive_Shift()
 {
     return (BalanceADCtype::Get() == BalanceADCtype::Hand);
 }
 
 
-static void OnChanged_ADC_Balance_ShiftA()
+static void OnChanged_ShiftA()
 {
     setNRST.balance_ADC[ChA] = shiftADCA;
     //    FPGA::WriteToHardware(WR_ADD_RSHIFT_DAC1, (uint8)BALANCE_ADC_A, false);
 }
 
 
-DEF_GOVERNOR(mgADC_Balance_ShiftA, PageDebug::PageADC::PageBalance::self,
+DEF_GOVERNOR(mgShiftA, PageDebug::PageADC::PageBalance::self,
     "Смещение 1", "Offset 1",
     "",
     "",
-    shiftADCA, -125, 125, IsActive_ADC_Balance_Shift, OnChanged_ADC_Balance_ShiftA, nullptr
+    shiftADCA, -125, 125, IsActive_Shift, OnChanged_ShiftA, nullptr
 )
 
 
-static void OnChanged_ADC_Balance_ShiftB()
+static void OnChanged_ShiftB()
 {
     setNRST.balance_ADC[ChB] = shiftADCB;
     //    FPGA::WriteToHardware(WR_ADD_RSHIFT_DAC2, (uint8)BALANCE_ADC_B, false);
 }
 
 
-DEF_GOVERNOR(mgADC_Balance_ShiftB, PageDebug::PageADC::PageBalance::self,
+DEF_GOVERNOR(mgShiftB, PageDebug::PageADC::PageBalance::self,
     "Смещение 2", "Offset 2",
     "",
     "",
-    shiftADCB, -125, 125, IsActive_ADC_Balance_Shift, OnChanged_ADC_Balance_ShiftB, nullptr
+    shiftADCB, -125, 125, IsActive_Shift, OnChanged_ShiftB, nullptr
 )
 
 
@@ -84,9 +84,9 @@ DEF_PAGE_3(pageBalanceADC, PageDebug::PageADC::self, NamePage::DebugADCbalance,
     "БАЛАНС", "BALANCE",
     "",
     "",
-    mcADC_Balance_Mode,     // ОТЛАДКА - АЦП - БАЛАНС - Режим
-    mgADC_Balance_ShiftA,   // ОТЛАДКА - АЦП - БАЛАНС - Смещение 1
-    mgADC_Balance_ShiftB,   // ОТЛАДКА - АЦП - БАЛАНС - Смещение 2
+    mcMode,     // ОТЛАДКА - АЦП - БАЛАНС - Режим
+    mgShiftA,   // ОТЛАДКА - АЦП - БАЛАНС - Смещение 1
+    mgShiftB,   // ОТЛАДКА - АЦП - БАЛАНС - Смещение 2
     nullptr, nullptr, nullptr, nullptr
 )
 
