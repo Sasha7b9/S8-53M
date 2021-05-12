e7a57931900f65ad7ff7464c3efc7f27f3a13afa
Sasha7b9 <Sasha7b9@tut.by>
2021-05-12 15:04:01 +0300

    refact

--- sources/Device/src/Menu/Pages/Memory/PageMemory.cpp
+++ sources/Device/src/Menu/Pages/Memory/PageMemory.cpp
@@ -87,42 +87,6 @@ void PressSB_MemLastSelect()
 
 
 
-static void DrawSB_MemExtSetNameSave(int x, int y)
-{
-    if (FDrive::isConnected)
-    {
-        Font::Set(TypeFont::UGO2);
-        Char('\x42').Draw4SymbolsInRect(x + 2, y + 1);
-        Font::Set(TypeFont::S8);
-    }
-}
-
-
-
-
-static void PressSB_SetName_Exit()
-{
-    Display::RemoveAddDrawFunction();
-    if (PageMemory::exitFromModeSetNameTo == RETURN_TO_LAST_MEM)
-    {
-        PageMemory::PageLatest::self->OpenAndSetItCurrent();
-    }
-    else if (PageMemory::exitFromModeSetNameTo == RETURN_TO_INT_MEM)
-    {
-        PageMemory::PageInternal::self->OpenAndSetItCurrent();
-    }
-    PageMemory::exitFromModeSetNameTo = RETURN_TO_DISABLE_MENU;
-}
-
-
-static void PressSB_MemExtSetNameSave()
-{
-    if (FDrive::isConnected)
-    {
-        PressSB_SetName_Exit();
-        PageMemory::needForSaveToFlashDrive = true;
-    }
-}
 
 
 
@@ -136,69 +100,6 @@ static void PressSB_MemExtSetNameSave()
 
 
 
-DEF_SMALL_BUTTON(sbSetNameSave, PageMemory::PageSetName::self,
-    "Сохранить", "Save",
-    "Сохранение на флеш под заданным именем",
-    "Saving to flashdrive with the specified name",
-    nullptr, PressSB_MemExtSetNameSave, DrawSB_MemExtSetNameSave, nullptr
-)
-
-
-
-void DrawSB_SetName_Backspace(int x, int y)
-{
-    Font::Set(TypeFont::UGO2);
-    Char(Symbol::UGO2::BACKSPACE).Draw4SymbolsInRect(x + 2, y + 1);
-    Font::Set(TypeFont::S8);
-}
-
-
-
-void PressSB_SetName_Backspace()
-{
-    int size = (int)(std::strlen(set.memory.file_name));
-    if (size > 0)
-    {
-        set.memory.file_name[size - 1] = '\0';
-    }
-}
-
-
-
-
-
-void DrawSB_SetName_Delete(int x, int y)
-{
-    Font::Set(TypeFont::UGO2);
-    Char(Symbol::UGO2::DEL).Draw4SymbolsInRect(x + 2, y + 1);
-    Font::Set(TypeFont::S8);
-}
-
-void PressSB_SetName_Delete()
-{
-    set.memory.file_name[0] = '\0';
-}
-
-
-
-
-
-void DrawSB_SetName_Insert(int x, int y)
-{
-    Font::Set(TypeFont::UGO2);
-    Char('\x26').Draw4SymbolsInRect(x + 2, y + 2);
-    Font::Set(TypeFont::S8);
-}
-
-void PressSB_SetName_Insert()
-{
-    int size = (int)(std::strlen(set.memory.file_name));
-    if (size < MAX_SYMBOLS_IN_FILE_NAME - 1)
-    {
-        set.memory.file_name[size] = Tables::symbolsAlphaBet[set.memory.index_cur_symbol_name_mask][0];
-        set.memory.file_name[size + 1] = '\0';
-    }
-}
 
 
 
@@ -210,26 +111,42 @@ void PressSB_SetName_Insert()
 
 
 
-DEF_SMALL_BUTTON(sbSetNameBackspace, PageMemory::PageSetName::self,
-    "Backspace", "Backspace",
-    "Удаляет последний символ",
-    "Delete the last character",
-    nullptr, PressSB_SetName_Backspace, DrawSB_SetName_Backspace, nullptr
-)
 
-DEF_SMALL_BUTTON(sbSetNameDelete, PageMemory::PageSetName::self,
-    "Удалить", "Delete",
-    "Удаляет все введённые символы",
-    "Deletes all entered characters",
-    nullptr, PressSB_SetName_Delete, DrawSB_SetName_Delete, nullptr
-)
 
-DEF_SMALL_BUTTON(sbSetNameInsert, PageMemory::PageSetName::self,
-    "Вставить", "Insert",
-    "Вводит очередной символ",
-    "Print the next character",
-    nullptr, PressSB_SetName_Insert, DrawSB_SetName_Insert, nullptr
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void DrawSB_MemExtNewFolder(int x, int y)
 {
@@ -337,12 +254,7 @@ DEF_SMALL_BUTTON(sbFileManagerLevelUp, PageMemory::PageFileManager::self,
 
 
 
-DEF_SMALL_BUTTON(sbExitSetName, PageMemory::PageSetName::self,     // Кнопк для выхода из режима задания имени сохраняемому сигналу. Одновременно кнопка отказа от сохранения
-    EXIT_RU, EXIT_EN,
-    "Отказ от сохранения",
-    "Failure to save",
-    nullptr, PressSB_SetName_Exit, DrawSB_Exit, nullptr
-)
+
 
 
 
@@ -513,23 +425,9 @@ DEF_PAGE_6(pageFileManager, PageMemory::PageExternal::self, NamePage::SB_FileMan
 
 
 // Страница вызывается при выбранном ручном режиме задания имени файла перед сохранением на флешку ///////////////
-static void OnMemExtSetNameRegSet(int angle)
-{
-    PageMemory::OnMemExtSetMaskNameRegSet(angle, sizeof(Tables::symbolsAlphaBet) / 4 - 7);
-}
 
-DEF_PAGE_6(pageSetName, &Page::empty, NamePage::SB_MemExtSetName,
-    "", "",
-    "",
-    "",
-    sbExitSetName,
-    sbSetNameDelete,
-    Item::empty,
-    sbSetNameBackspace,
-    sbSetNameInsert,
-    sbSetNameSave,
-    nullptr, nullptr, nullptr, OnMemExtSetNameRegSet
-)
+
+
 
 DEF_PAGE_4(pageMemory, PageMain::self, NamePage::Memory,
     "ПАМЯТЬ", "MEMORY",
@@ -543,5 +441,4 @@ DEF_PAGE_4(pageMemory, PageMain::self, NamePage::Memory,
 )
 
 const Page *PageMemory::self = &pageMemory;
-const Page *PageMemory::PageSetName::self = &pageSetName;
 const Page *PageMemory::PageFileManager::self = &pageFileManager;
---
+++ sources/Device/src/Menu/Pages/Memory/PageSetName.cpp
@@ -0,0 +1,158 @@
+// 2021/05/12 14:32:48 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "common/Display/Font/Font_.h"
+#include "FDrive/FDrive.h"
+#include "Menu/Items/MenuItems.h"
+#include "Menu/Items/MenuItemsDefs.h"
+#include "Menu/Pages/Definition.h"
+#include "Settings/Settings.h"
+#include <cstring>
+
+
+static void PressSB_SetName_Exit()
+{
+    Display::RemoveAddDrawFunction();
+    if (PageMemory::exitFromModeSetNameTo == RETURN_TO_LAST_MEM)
+    {
+        PageMemory::PageLatest::self->OpenAndSetItCurrent();
+    }
+    else if (PageMemory::exitFromModeSetNameTo == RETURN_TO_INT_MEM)
+    {
+        PageMemory::PageInternal::self->OpenAndSetItCurrent();
+    }
+    PageMemory::exitFromModeSetNameTo = RETURN_TO_DISABLE_MENU;
+}
+
+
+DEF_SMALL_BUTTON(sbExitSetName, PageMemory::PageSetName::self,     // Кнопк для выхода из режима задания имени сохраняемому сигналу. Одновременно кнопка отказа от сохранения
+    EXIT_RU, EXIT_EN,
+    "Отказ от сохранения",
+    "Failure to save",
+    nullptr, PressSB_SetName_Exit, DrawSB_Exit, nullptr
+)
+
+
+void PressSB_SetName_Delete()
+{
+    set.memory.file_name[0] = '\0';
+}
+
+
+void DrawSB_SetName_Delete(int x, int y)
+{
+    Font::Set(TypeFont::UGO2);
+    Char(Symbol::UGO2::DEL).Draw4SymbolsInRect(x + 2, y + 1);
+    Font::Set(TypeFont::S8);
+}
+
+
+DEF_SMALL_BUTTON(sbSetNameDelete, PageMemory::PageSetName::self,
+    "Удалить", "Delete",
+    "Удаляет все введённые символы",
+    "Deletes all entered characters",
+    nullptr, PressSB_SetName_Delete, DrawSB_SetName_Delete, nullptr
+)
+
+
+void PressSB_SetName_Backspace()
+{
+    int size = (int)(std::strlen(set.memory.file_name));
+    if (size > 0)
+    {
+        set.memory.file_name[size - 1] = '\0';
+    }
+}
+
+
+void DrawSB_SetName_Backspace(int x, int y)
+{
+    Font::Set(TypeFont::UGO2);
+    Char(Symbol::UGO2::BACKSPACE).Draw4SymbolsInRect(x + 2, y + 1);
+    Font::Set(TypeFont::S8);
+}
+
+
+DEF_SMALL_BUTTON(sbSetNameBackspace, PageMemory::PageSetName::self,
+    "Backspace", "Backspace",
+    "Удаляет последний символ",
+    "Delete the last character",
+    nullptr, PressSB_SetName_Backspace, DrawSB_SetName_Backspace, nullptr
+)
+
+
+void PressSB_SetName_Insert()
+{
+    int size = (int)(std::strlen(set.memory.file_name));
+    if (size < MAX_SYMBOLS_IN_FILE_NAME - 1)
+    {
+        set.memory.file_name[size] = Tables::symbolsAlphaBet[set.memory.index_cur_symbol_name_mask][0];
+        set.memory.file_name[size + 1] = '\0';
+    }
+}
+
+
+void DrawSB_SetName_Insert(int x, int y)
+{
+    Font::Set(TypeFont::UGO2);
+    Char('\x26').Draw4SymbolsInRect(x + 2, y + 2);
+    Font::Set(TypeFont::S8);
+}
+
+
+DEF_SMALL_BUTTON(sbSetNameInsert, PageMemory::PageSetName::self,
+    "Вставить", "Insert",
+    "Вводит очередной символ",
+    "Print the next character",
+    nullptr, PressSB_SetName_Insert, DrawSB_SetName_Insert, nullptr
+)
+
+
+static void PressSB_MemExtSetNameSave()
+{
+    if (FDrive::isConnected)
+    {
+        PressSB_SetName_Exit();
+        PageMemory::needForSaveToFlashDrive = true;
+    }
+}
+
+
+static void DrawSB_MemExtSetNameSave(int x, int y)
+{
+    if (FDrive::isConnected)
+    {
+        Font::Set(TypeFont::UGO2);
+        Char('\x42').Draw4SymbolsInRect(x + 2, y + 1);
+        Font::Set(TypeFont::S8);
+    }
+}
+
+
+DEF_SMALL_BUTTON(sbSetNameSave, PageMemory::PageSetName::self,
+    "Сохранить", "Save",
+    "Сохранение на флеш под заданным именем",
+    "Saving to flashdrive with the specified name",
+    nullptr, PressSB_MemExtSetNameSave, DrawSB_MemExtSetNameSave, nullptr
+)
+
+
+static void OnMemExtSetNameRegSet(int angle)
+{
+    PageMemory::OnMemExtSetMaskNameRegSet(angle, sizeof(Tables::symbolsAlphaBet) / 4 - 7);
+}
+
+
+DEF_PAGE_6(pageSetName, &Page::empty, NamePage::SB_MemExtSetName,
+    "", "",
+    "",
+    "",
+    sbExitSetName,
+    sbSetNameDelete,
+    Item::empty,
+    sbSetNameBackspace,
+    sbSetNameInsert,
+    sbSetNameSave,
+    nullptr, nullptr, nullptr, OnMemExtSetNameRegSet
+)
+
+const Page *PageMemory::PageSetName::self = &pageSetName;
