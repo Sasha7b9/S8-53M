cb9d057af1375f6bb0d0d9f6a0437555b7abbc4b
Sasha7b9 <Sasha7b9@tut.by>
2021-06-16 16:29:27 +0300

    Пины ETH задействованы

--- sources/common/Hardware/HAL/HAL_PINS_.cpp
+++ sources/common/Hardware/HAL/HAL_PINS_.cpp
@@ -200,23 +200,23 @@ void HAL_PINS::Init()
     PinOTG_FS(A, 11).Init();    // DM
     PinOTG_FS(A, 9).Init();     // VBUS
 
-//    PinETH(H, 2).Init();        // CRS
-//    PinETH(A, 2).Init();        // MDIO
-//    PinETH(H, 3).Init();        // COL
-//    PinETH(A, 7).Init();        // RX_DV
-//    PinETH(H, 6).Init();        // RXD2
-//    PinETH(B, 1).Init();        // RXD3
-//    PinETH(B, 8).Init();        // TXD3
-//    PinETH(I, 10).Init();       // RX_ER
-//    PinETH(B, 11).Init();       // TX_EN
-//    PinETH(C, 1).Init();        // MDC
-//    PinETH(C, 2).Init();        // TXD2
-//    PinETH(C, 3).Init();        // TX_CLK
-//    PinETH(A, 1).Init();        // RX_CLK
-//    PinETH(C, 4).Init();        // RXD0
-//    PinETH(C, 5).Init();        // RXD1
-//    PinETH(B, 12).Init();       // TXD0
-//    PinETH(G, 14).Init();       // TXD1
+    PinETH(H, 2).Init();        // CRS
+    PinETH(A, 2).Init();        // MDIO
+    PinETH(H, 3).Init();        // COL
+    PinETH(A, 7).Init();        // RX_DV
+    PinETH(H, 6).Init();        // RXD2
+    PinETH(B, 1).Init();        // RXD3
+    PinETH(B, 8).Init();        // TXD3
+    PinETH(I, 10).Init();       // RX_ER
+    PinETH(B, 11).Init();       // TX_EN
+    PinETH(C, 1).Init();        // MDC
+    PinETH(C, 2).Init();        // TXD2
+    PinETH(C, 3).Init();        // TX_CLK
+    PinETH(A, 1).Init();        // RX_CLK
+    PinETH(C, 4).Init();        // RXD0
+    PinETH(C, 5).Init();        // RXD1
+    PinETH(B, 12).Init();       // TXD0
+    PinETH(G, 14).Init();       // TXD1
 
     Pin(PinMode::_DAC1, PinPort::_A, PinPin::_4).Init();        // PWM
 
