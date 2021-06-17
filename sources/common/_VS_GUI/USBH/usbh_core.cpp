b30196c6c99aa60a3b4c2a09cbccf26ddfdadc8f
Sasha7b9 <Sasha7b9@tut.by>
2021-06-17 16:01:48 +0300

    *

--- sources/common/_VS_GUI/USBH/usbh_core.cpp
+++ sources/common/_VS_GUI/USBH/usbh_core.cpp
@@ -6,3 +6,25 @@ USBH_StatusTypeDef  USBH_Process(USBH_HandleTypeDef *)
 {
     return USBH_FAIL;
 }
+
+
+USBH_StatusTypeDef  USBH_LL_Connect(USBH_HandleTypeDef *)
+{
+    return USBH_OK;
+}
+
+
+USBH_StatusTypeDef  USBH_LL_Disconnect(USBH_HandleTypeDef *)
+{
+    return USBH_OK;
+}
+
+
+void  USBH_LL_SetTimer(USBH_HandleTypeDef *, uint32_t)
+{
+}
+
+
+void  USBH_LL_IncTimer(USBH_HandleTypeDef *)
+{
+}
